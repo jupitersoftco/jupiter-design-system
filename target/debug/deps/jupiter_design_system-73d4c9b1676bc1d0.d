aa528a38ac795709fbf9f731d50b6da3309e62ae
Matthew Elder <matt@jupitersoft.net>
2025-07-11 22:40:30 -0700

    chore: cleanup

--- target/debug/deps/jupiter_design_system-73d4c9b1676bc1d0.d
+++ target/debug/deps/jupiter_design_system-73d4c9b1676bc1d0.d
@@ -1,16 +1,21 @@
-/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/jupiter_design_system-73d4c9b1676bc1d0.d: src/lib.rs src/components/mod.rs src/components/button.rs src/core/mod.rs src/core/color.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/themes/mod.rs src/utils/mod.rs
+/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/jupiter_design_system-73d4c9b1676bc1d0.d: src/lib.rs src/builders/mod.rs src/builders/button.rs src/core/mod.rs src/core/color.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/patterns/mod.rs src/patterns/actions.rs src/patterns/button.rs src/patterns/focus.rs src/patterns/interactions.rs src/themes/mod.rs src/utils/mod.rs
 
-/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/libjupiter_design_system-73d4c9b1676bc1d0.rlib: src/lib.rs src/components/mod.rs src/components/button.rs src/core/mod.rs src/core/color.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/themes/mod.rs src/utils/mod.rs
+/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/libjupiter_design_system-73d4c9b1676bc1d0.rlib: src/lib.rs src/builders/mod.rs src/builders/button.rs src/core/mod.rs src/core/color.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/patterns/mod.rs src/patterns/actions.rs src/patterns/button.rs src/patterns/focus.rs src/patterns/interactions.rs src/themes/mod.rs src/utils/mod.rs
 
-/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/libjupiter_design_system-73d4c9b1676bc1d0.rmeta: src/lib.rs src/components/mod.rs src/components/button.rs src/core/mod.rs src/core/color.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/themes/mod.rs src/utils/mod.rs
+/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/libjupiter_design_system-73d4c9b1676bc1d0.rmeta: src/lib.rs src/builders/mod.rs src/builders/button.rs src/core/mod.rs src/core/color.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/patterns/mod.rs src/patterns/actions.rs src/patterns/button.rs src/patterns/focus.rs src/patterns/interactions.rs src/themes/mod.rs src/utils/mod.rs
 
 src/lib.rs:
-src/components/mod.rs:
-src/components/button.rs:
+src/builders/mod.rs:
+src/builders/button.rs:
 src/core/mod.rs:
 src/core/color.rs:
 src/core/sizing.rs:
 src/core/spacing.rs:
 src/core/typography.rs:
+src/patterns/mod.rs:
+src/patterns/actions.rs:
+src/patterns/button.rs:
+src/patterns/focus.rs:
+src/patterns/interactions.rs:
 src/themes/mod.rs:
 src/utils/mod.rs:
--- target/debug/deps/jupiter_design_system-a3edaa496379c924.d
+++ target/debug/deps/jupiter_design_system-a3edaa496379c924.d
@@ -1,16 +1,21 @@
-/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/jupiter_design_system-a3edaa496379c924.d: src/lib.rs src/components/mod.rs src/components/button.rs src/components/button_test.rs src/core/mod.rs src/core/color.rs src/core/color_test.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/themes/mod.rs src/utils/mod.rs
+/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/jupiter_design_system-a3edaa496379c924.d: src/lib.rs src/builders/mod.rs src/builders/button.rs src/builders/button_test.rs src/core/mod.rs src/core/color.rs src/core/color_test.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/patterns/mod.rs src/patterns/actions.rs src/patterns/button.rs src/patterns/focus.rs src/patterns/interactions.rs src/themes/mod.rs src/utils/mod.rs
 
-/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/jupiter_design_system-a3edaa496379c924: src/lib.rs src/components/mod.rs src/components/button.rs src/components/button_test.rs src/core/mod.rs src/core/color.rs src/core/color_test.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/themes/mod.rs src/utils/mod.rs
+/Users/anon/dev/jupiter-startups/jupiter-design-system/target/debug/deps/jupiter_design_system-a3edaa496379c924: src/lib.rs src/builders/mod.rs src/builders/button.rs src/builders/button_test.rs src/core/mod.rs src/core/color.rs src/core/color_test.rs src/core/sizing.rs src/core/spacing.rs src/core/typography.rs src/patterns/mod.rs src/patterns/actions.rs src/patterns/button.rs src/patterns/focus.rs src/patterns/interactions.rs src/themes/mod.rs src/utils/mod.rs
 
 src/lib.rs:
-src/components/mod.rs:
-src/components/button.rs:
-src/components/button_test.rs:
+src/builders/mod.rs:
+src/builders/button.rs:
+src/builders/button_test.rs:
 src/core/mod.rs:
 src/core/color.rs:
 src/core/color_test.rs:
 src/core/sizing.rs:
 src/core/spacing.rs:
 src/core/typography.rs:
+src/patterns/mod.rs:
+src/patterns/actions.rs:
+src/patterns/button.rs:
+src/patterns/focus.rs:
+src/patterns/interactions.rs:
 src/themes/mod.rs:
 src/utils/mod.rs:
